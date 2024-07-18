e18fe6fec6d137676b9e564b6f8da9a8d4d2b7ad
a-cyborg <gx_339-4@pm.me>
2024-07-18 17:48:35 +0900

    Add AlarmTImeUtil

--- alarm-service/build.gradle.kts
+++ alarm-service/build.gradle.kts
@@ -4,11 +4,36 @@ plugins {
 
 android {
     namespace = "org.a_cyb.sayitalarm.alarm_service"
+
+    compileOptions {
+        isCoreLibraryDesugaringEnabled = true
+    }
+
+    @Suppress("UnstableApiUsage")
+    testOptions {
+        unitTests {
+            isReturnDefaultValues = true
+            isIncludeAndroidResources = true
+            all { it.jvmArgs("--add-opens", "java.base/java.time=ALL-UNNAMED") }
+        }
+    }
 }
 
-dependencies {
-    implementation(projects.domain.alarmService)
 
+dependencies {
+    implementation(projects.entity)
+    implementation(projects.domain.alarmService)
+    implementation(projects.domain.repository)
+
+    coreLibraryDesugaring(libs.android.tools.desugarJdk)
+    implementation(libs.androidx.work.workRuntime)
     implementation(libs.kotlinx.coroutines.core)
-    implementation(libs.koin.core)
+    implementation(libs.koin.android)
+
+    // Debug
+    testImplementation(libs.androidx.test.ext)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.robolectric)
+    testImplementation(libs.mockk)
+    testImplementation(libs.androidx.work.workTesting)
 }
---
+++ alarm-service/src/main/kotlin/org/a_cyb/sayitalarm/alarm_service/AlarmTImeUtil.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2024 Mima Kang / All rights reserved.
+ *
+ * Use of this source code is governed by Apache v2.0
+ */
+
+package org.a_cyb.sayitalarm.alarm_service
+
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
+import java.time.temporal.TemporalAdjusters
+import org.a_cyb.sayitalarm.entity.Hour
+import org.a_cyb.sayitalarm.entity.Minute
+import org.a_cyb.sayitalarm.entity.WeeklyRepeat
+
+fun getNextAlarmTime(hour: Hour, minute: Minute, weeklyRepeat: WeeklyRepeat): LocalDateTime {
+    val alarmTime = LocalTime.of(hour.hour, minute.minute)
+    val alarmDate = getNextDayOfAlarm(alarmTime, weeklyRepeat)
+    val alarmDateTime = LocalDateTime.of(alarmDate, alarmTime)
+
+    return alarmDateTime.withSecond(0).withNano(0)
+}
+
+private fun getNextDayOfAlarm(alarmTime: LocalTime, weeklyRepeat: WeeklyRepeat): LocalDate {
+    val nowDate = LocalDate.now()
+    val nowTime = LocalTime.now()
+    val todayCode = nowDate.dayOfWeek.value
+
+    // The alarm time for today is still upcoming.
+    if (alarmTime.isAfter(nowTime) &&
+        (weeklyRepeat.weekdays.contains(todayCode) || !weeklyRepeat.isRepeat())
+    ) {
+        return LocalDate.now()
+    }
+
+    // The alarm time for today has already passed.
+    return if (!weeklyRepeat.isRepeat()) {
+        nowDate.plusDays(1)
+    } else {
+        // If no day is available this week, select the first available date next week.
+        val nextDayCode = weeklyRepeat.weekdays
+            .firstOrNull { it > todayCode }
+            ?: weeklyRepeat.weekdays.first()
+
+        nowDate
+            .with(TemporalAdjusters.next(DayOfWeek.of(nextDayCode)))
+    }
+}
+
+private fun WeeklyRepeat.isRepeat(): Boolean =
+    weekdays.isNotEmpty()
