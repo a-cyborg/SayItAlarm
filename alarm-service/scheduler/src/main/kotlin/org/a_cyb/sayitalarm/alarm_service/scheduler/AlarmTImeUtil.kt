42a0828cf1d0e307674980302f746733b6250353
a-cyborg <gx_339-4@pm.me>
2024-11-16 21:40:24 +0900

    Refactor AlarmTImeUtil

--- alarm-service/scheduler/src/main/kotlin/org/a_cyb/sayitalarm/alarm_service/scheduler/AlarmTImeUtil.kt
+++ alarm-service/scheduler/src/main/kotlin/org/a_cyb/sayitalarm/alarm_service/scheduler/AlarmTImeUtil.kt
@@ -17,15 +17,12 @@ import org.a_cyb.sayitalarm.entity.Minute
 import org.a_cyb.sayitalarm.entity.WeeklyRepeat
 
 fun getSnoozeTimeInMills(snoozeMin: Int): Long =
-    LocalDateTime.now()
-        .plusMinutes(snoozeMin.toLong())
-        .toZonedMilliSec()
+    LocalDateTime.now().plusMinutes(snoozeMin.toLong()).toZonedMilliSec()
 
 fun getNextAlarmTimeInMills(hour: Hour, minute: Minute, weeklyRepeat: WeeklyRepeat): Long =
-    getNextAlarmTime(LocalTime.of(hour.hour, minute.minute), weeklyRepeat)
-        .toZonedMilliSec()
+    getNextAlarmTime(LocalTime.of(hour.hour, minute.minute), weeklyRepeat).toZonedMilliSec()
 
-fun getNextAlarmTime(alarmTime: LocalTime, weeklyRepeat: WeeklyRepeat): LocalDateTime {
+private fun getNextAlarmTime(alarmTime: LocalTime, weeklyRepeat: WeeklyRepeat): LocalDateTime {
     val alarmDate = getNextDateOfAlarm(alarmTime, weeklyRepeat)
 
     return LocalDateTime.of(alarmDate, alarmTime).withSecond(0).withNano(0)
@@ -36,20 +33,19 @@ private fun getNextDateOfAlarm(alarmTime: LocalTime, weeklyRepeat: WeeklyRepeat)
     val nowTime = LocalTime.now()
     val todayCode = nowDate.dayOfWeek.value
 
-    // The alarm time for today is still upcoming.
-    if (alarmTime.isAfter(nowTime) && (weeklyRepeat.weekdays.contains(todayCode) || !weeklyRepeat.isRepeat())) {
-        return nowDate
-    }
-
-    // The alarm time for today has already passed.
-    return if (!weeklyRepeat.isRepeat()) {
-        nowDate.plusDays(1)
+    return if (alarmTime.isAfter(nowTime) && (weeklyRepeat.weekdays.contains(todayCode) || !weeklyRepeat.isRepeat())) {
+        // The alarm time for today is still upcoming.
+        nowDate
     } else {
-        // If all repeating days have passed this week, then the first repeating day in the next week.
-        val nextDayCode = weeklyRepeat.weekdays
-            .firstOrNull { it > todayCode } ?: weeklyRepeat.weekdays.first()
+        return when (weeklyRepeat.isRepeat()) {
+            true -> {
+                // If all repeating days have passed this week, then the first repeating day in the next week.
+                val nextDayCode = weeklyRepeat.weekdays.firstOrNull { it > todayCode } ?: weeklyRepeat.weekdays.first()
+                nowDate.with(TemporalAdjusters.next(DayOfWeek.of(nextDayCode)))
+            }
 
-        nowDate.with(TemporalAdjusters.next(DayOfWeek.of(nextDayCode)))
+            false -> nowDate.plusDays(1)
+        }
     }
 }
 
--- alarm-service/scheduler/src/test/kotlin/org/a_cyb/sayitalarm/alarm_service/scheduler/AlarmTimeUtilSpec.kt
+++ alarm-service/scheduler/src/test/kotlin/org/a_cyb/sayitalarm/alarm_service/scheduler/AlarmTimeUtilSpec.kt
@@ -11,13 +11,17 @@ import io.mockk.every
 import io.mockk.mockkStatic
 import io.mockk.unmockkAll
 import java.time.DayOfWeek
+import java.time.Instant
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.LocalTime
+import java.time.ZoneId
 import kotlin.test.Test
-import kotlin.test.assertEquals
+import org.a_cyb.sayitalarm.entity.Hour
+import org.a_cyb.sayitalarm.entity.Minute
 import org.a_cyb.sayitalarm.entity.WeeklyRepeat
 import org.junit.After
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.runner.RunWith
 
@@ -34,76 +38,159 @@ class AlarmTimeUtilSpec {
     }
 
     @Test
-    fun getNextAlarmTime_RepeatAlarm_AlarmTimeForToday_HasPassed_AvailableRepeatDayInCurrentWeek() {
+    fun getNextAlarmTimeInMills_RepeatAlarm_AlarmTimeForTodayHasPassed_AvailableRepeatDayInCurrentWeek() {
         // Given
-        val alarmTime = LocalTime.of(13, 33)
-        val alarmWeeklyRepeat = WeeklyRepeat(DayOfWeek.MONDAY.value, DayOfWeek.WEDNESDAY.value, DayOfWeek.FRIDAY.value)
-
+        val alarmWeeklyRepeat = WeeklyRepeat(DayOfWeek.MONDAY, DayOfWeek.WEDNESDAY, DayOfWeek.FRIDAY)
         // Current clock: 2024-July-18(Thursday) 3:33
         every { LocalDate.now() } returns LocalDate.of(2024, 7, 18)
         every { LocalTime.now() } returns LocalTime.of(3, 33)
 
         // When
-        val actual = getNextAlarmTime(alarmTime, alarmWeeklyRepeat)
+        val actual = getNextAlarmTimeInMills(Hour(13), Minute(33), alarmWeeklyRepeat)
 
         // Then
-        val expectedDate = LocalDate.of(2024, 7, 19)
-        val expected = LocalDateTime.of(expectedDate, alarmTime)
-
-        assertEquals(expected, actual)
+        val actualInstant = Instant.ofEpochMilli(actual).atZone(ZoneId.systemDefault())
+        // Expected: 2024-July-19(Friday) 3:33
+        assertEquals(2024, actualInstant.year)
+        assertEquals(7, actualInstant.month.value)
+        assertEquals(19, actualInstant.dayOfMonth)
+        assertEquals(DayOfWeek.FRIDAY, actualInstant.dayOfWeek)
+        assertEquals(13, actualInstant.hour)
+        assertEquals(33, actualInstant.minute)
+        assertEquals(0, actualInstant.second)
+        assertEquals(0, actualInstant.nano)
     }
 
     @Test
-    fun getNextAlarmTime_RepeatAlarm_AlarmTimeForToday_HasPassed_NoAvailableRepeatDayInCurrentWeek() {
+    fun getNextAlarmTime_RepeatAlarm_AlarmTimeForTodayHasPassed_NoAvailableRepeatDayInCurrentWeek() {
         // Given
-        val alarmTime = LocalTime.of(7, 0)
-        val alarmWeeklyRepeat = WeeklyRepeat(DayOfWeek.THURSDAY.value)
-
+        val alarmWeeklyRepeat = WeeklyRepeat(DayOfWeek.THURSDAY)
         // Current clock: 2024-July-18(Thursday) 9:00
         every { LocalDate.now() } returns LocalDate.of(2024, 7, 18)
         every { LocalTime.now() } returns LocalTime.of(9, 0)
 
         // When
-        val actual = getNextAlarmTime(alarmTime, alarmWeeklyRepeat)
-        val expected = LocalDateTime.of(2024, 7, 25, 7, 0)
+        val actual = getNextAlarmTimeInMills(Hour(7), Minute(0), alarmWeeklyRepeat)
 
         // Then
-        assertEquals(expected, actual)
+        val actualInstant = Instant.ofEpochMilli(actual).atZone(ZoneId.systemDefault())
+        // Expected clock: 2024-July-25(Thursday) 7:00
+        assertEquals(2024, actualInstant.year)
+        assertEquals(7, actualInstant.month.value)
+        assertEquals(25, actualInstant.dayOfMonth)
+        assertEquals(DayOfWeek.THURSDAY, actualInstant.dayOfWeek)
+        assertEquals(7, actualInstant.hour)
+        assertEquals(0, actualInstant.minute)
+        assertEquals(0, actualInstant.second)
+        assertEquals(0, actualInstant.nano)
     }
 
     @Test
     fun getNextAlarmTime_RepeatAlarm_AlarmTimeForToday_HasNotYetPassed() {
         // Given
-        val alarmTime = LocalTime.of(7, 0)
-        val alarmWeeklyRepeat = WeeklyRepeat(DayOfWeek.THURSDAY.value)
+        val weeklyRepeat = WeeklyRepeat(DayOfWeek.WEDNESDAY)
 
-        // Current clock: 2024-July-28(Thursday) 6:00
-        every { LocalDate.now() } returns LocalDate.of(2024, 7, 18)
-        every { LocalTime.now() } returns LocalTime.of(6, 0)
+        // Current clock: 2024-July-17(Wednesday) 5:00
+        every { LocalDate.now() } returns LocalDate.of(2024, 7, 17)
+        every { LocalTime.now() } returns LocalTime.of(5, 0)
 
         // When
-        val actual = getNextAlarmTime(alarmTime, alarmWeeklyRepeat)
-        val expected = LocalDateTime.of(2024, 7, 18, 7, 0)
+        val actual = getNextAlarmTimeInMills(Hour(5), Minute(33), weeklyRepeat)
 
         // Then
-        assertEquals(expected, actual)
+        val actualInstant = Instant.ofEpochMilli(actual).atZone(ZoneId.systemDefault())
+        // Expected: 2024-July-17(Wednesday) 5:33
+        assertEquals(2024, actualInstant.year)
+        assertEquals(7, actualInstant.month.value)
+        assertEquals(17, actualInstant.dayOfMonth)
+        assertEquals(DayOfWeek.WEDNESDAY, actualInstant.dayOfWeek)
+        assertEquals(5, actualInstant.hour)
+        assertEquals(33, actualInstant.minute)
     }
 
     @Test
     fun getNextAlarmTime_NoRepeatAlarm_AlarmTimeForToday_HasPassed() {
         // Given
-        val alarmTime = LocalTime.of(7, 0)
-        val alarmWeeklyRepeat = WeeklyRepeat()
-
-        // Current clock: 2024-July-18(Thursday) 9:00
-        every { LocalDate.now() } returns LocalDate.of(2024, 7, 18)
+        // Current clock: 2024-November-17(Sunday) 9:00
+        every { LocalDate.now() } returns LocalDate.of(2024, 11, 17)
         every { LocalTime.now() } returns LocalTime.of(9, 0)
 
         // When
-        val actual = getNextAlarmTime(alarmTime, alarmWeeklyRepeat)
-        val expected = LocalDateTime.of(2024, 7, 19, 7, 0)
+        val actual = getNextAlarmTimeInMills(Hour(8), Minute(3), WeeklyRepeat())
 
         // Then
-        assertEquals(expected, actual)
+        val actualInstant = Instant.ofEpochMilli(actual).atZone(ZoneId.systemDefault())
+        // Expected: 2024-November-18(Monday) 8:03
+        assertEquals(2024, actualInstant.year)
+        assertEquals(11, actualInstant.month.value)
+        assertEquals(18, actualInstant.dayOfMonth)
+        assertEquals(DayOfWeek.MONDAY, actualInstant.dayOfWeek)
+        assertEquals(8, actualInstant.hour)
+        assertEquals(3, actualInstant.minute)
+    }
+
+    @Test
+    fun getNextAlarmTime_NoRepeatAlarm_AlarmTimeForToday_HasNotYetPassed() {
+        // Given
+        // Current clock: 2024-November-17(Sunday) 6:00
+        every { LocalDate.now() } returns LocalDate.of(2024, 11, 17)
+        every { LocalTime.now() } returns LocalTime.of(6, 0)
+
+        // When
+        val actual = getNextAlarmTimeInMills(Hour(8), Minute(3), WeeklyRepeat())
+
+        // Then
+        val actualInstant = Instant.ofEpochMilli(actual).atZone(ZoneId.systemDefault())
+        // Expected: 2024-November-17(Monday) 8:03
+        assertEquals(2024, actualInstant.year)
+        assertEquals(11, actualInstant.month.value)
+        assertEquals(17, actualInstant.dayOfMonth)
+        assertEquals(DayOfWeek.SUNDAY, actualInstant.dayOfWeek)
+        assertEquals(8, actualInstant.hour)
+        assertEquals(3, actualInstant.minute)
+    }
+
+    @Test
+    fun getSnoozeTimeInMills_5min() {
+        // Given
+        mockkStatic(LocalDateTime::class)
+
+        // Current clock: 2024-December-3(Tuesday) 14:20
+        every { LocalDateTime.now() } returns LocalDateTime.of(2024, 12, 3, 14, 20)
+
+        // When
+        val actual = getSnoozeTimeInMills(5)
+
+        // Then
+        val actualInstant = Instant.ofEpochMilli(actual).atZone(ZoneId.systemDefault())
+        // Expected: 2024-December-3(Tuesday) 14:25
+        assertEquals(2024, actualInstant.year)
+        assertEquals(12, actualInstant.month.value)
+        assertEquals(3, actualInstant.dayOfMonth)
+        assertEquals(14, actualInstant.hour)
+        assertEquals(25, actualInstant.minute)
+        assertEquals(0, actualInstant.second)
+        assertEquals(0, actualInstant.nano)
+    }
+
+    @Test
+    fun getSnoozeTimeInMills_15min() {
+        // Given
+        mockkStatic(LocalDateTime::class)
+
+        // Current clock: 2024-December-3(Tuesday) 7:50
+        every { LocalDateTime.now() } returns LocalDateTime.of(2024, 12, 3, 7, 50)
+
+        // When
+        val actual = getSnoozeTimeInMills(15)
+
+        // Then
+        val actualInstant = Instant.ofEpochMilli(actual).atZone(ZoneId.systemDefault())
+        // Expected: 2024-December-3(Tuesday) 8:05
+        assertEquals(2024, actualInstant.year)
+        assertEquals(12, actualInstant.month.value)
+        assertEquals(3, actualInstant.dayOfMonth)
+        assertEquals(8, actualInstant.hour)
+        assertEquals(5, actualInstant.minute)
     }
 }
