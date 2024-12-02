bdabb7cd9903f9966361fea045649b9e49f70bc4
a-cyborg <gx_339-4@pm.me>
2024-12-02 20:57:57 +0900

    Add SayIt entity

---
+++ entity/src/main/kotlin/org/a_cyb/sayitalarm/entity/SayIt.kt
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2024 Mima Kang / All rights reserved.
+ *
+ * Use of this source code is governed by Apache v2.0
+ */
+
+package org.a_cyb.sayitalarm.entity
+
+data class SayIt(
+    val script: String,
+    val transcript: String,
+)
